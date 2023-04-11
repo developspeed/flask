b5c4f239759eb18a04936658d4e5b99d5b854547
developspeed <gerard@dataspeed.nl>
2023-04-12 04:11:14 +0530

    new changes

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 from flask import Flask, render_template, request, redirect, session, url_for
 import mysql.connector as ms
+import os
+import replicate
+from io import BytesIO
 
 
-app = Flask(__name__)
+# Setting the Environment of the application using the API token of Replicate from the database
+cnx = ms.connect(user='magic_register', password='Indira@2000',
+                 host='185.104.29.84', database='magic_register')
+cursor = cnx.cursor()
+get_API_key_query = "SELECT `API_KEY` FROM `whisper_config`"
+cursor.execute(get_API_key_query)
+get_API_key = ''
+for apiKEY in cursor:
+    get_API_key = apiKEY[0]
+# print(get_API_key)
+os.environ['REPLICATE_API_TOKEN'] = str(get_API_key)
+
+# Closing the DB Connection
+cursor.close()
+cnx.close()
+
+app = Flask(__name__, static_url_path='/static')
 app.secret_key = 'my-secret-key'
 
 
@@ -85,20 +104,165 @@ def Dashboard():
 
         cursor.close()
         cnx.close()
-        return render_template("dashboard.html", data={'name': name, 'subscription_end': subscription_end, 'words_total': words_total, 'words_count': words_count, 'images_total':images_total, 'images_count':images_count})
+        return render_template("dashboard.html", data={'name': name, 'subscription_end': subscription_end, 'words_total': words_total, 'words_count': words_count, 'images_total': images_total, 'images_count': images_count, 'minutes_total':minutes_total, 'minutes_count':minutes_count})
 
-    else:     
+    else:
         return redirect(url_for("login"))
+
+
+minutes_count = 0
+minutes_total = 0
+
+
+@app.route('/whisper')
+def Whisper():
+    if 'get_user_email' in session:
+        cnx = ms.connect(user='magic_register', password='Indira@2000',
+                         host='185.104.29.84', database='magic_register')
+        cursor = cnx.cursor()
+
+        global minutes_count, minutes_total
+        email = session['get_user_email']
+        minutes_count_query = f"SELECT `minutes_count` FROM `user` WHERE `email` = '{email}'"
+        cursor.execute(minutes_count_query)
+        minutes_count = float(cursor.fetchone()[0])
+
+        minutes_total_query = f"SELECT `minutes_total` FROM `user` WHERE `email` = '{email}'"
+        cursor.execute(minutes_total_query)
+        minutes_total = cursor.fetchone()[0]
+
+        cursor.close()
+        cnx.close()
+        return render_template('whisper.html', data=[minutes_count, minutes_total])
+
+    else:
+        return redirect(url_for("login"))
+
+
+audioRecordedGlobal = None
+# Audio Upload through Mic
+
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    audio = request.files.get('audio').read()
+    # print(audio)
+    global audioRecordedGlobal
+    audioRecordedGlobal = audio
+    return "Done"
+
+
+# function to convert the information into
+# some readable format
+def audio_duration(length):
+    hours = length / 3600  # calculate in hours
+    length %= 3600
+    mins = length / 60  # calculate in minutes
+    length %= 60
+    seconds = length  # calculate in seconds
+
+    return mins  # returns the duration
+
+
+@app.route('/whisper-results', methods=["POST"])
+def WhisperAI():
+    cnx = ms.connect(user='magic_register', password='Indira@2000',
+                     host='185.104.29.84', database='magic_register')
+    cursor = cnx.cursor()
+    email = session['get_user_email']
+    # Reading the audio file and Converting the audio file in Bytes
+
+    audioFile = BytesIO(request.files['audioFile'].read())
+    # Or
+    # Taking mic input
+    audioRecords = BytesIO(audioRecordedGlobal)
+    # print(audioRecordedGlobal)
+
+    language = request.form['language']
+    to_translate = request.form.get('to_translate') == 'on'
+
+    if minutes_count <= float(minutes_total):
+        if len(audioFile.read()) != 0:
+
+            # Counting audio file minutes
+            from mutagen.wave import WAVE
+            audios = WAVE(audioFile)
+            audio_length = audios.info.length
+            minutes = str(audio_duration(audio_length))
+            minutes_to_update = float(minutes[0:4])
+            print(minutes_to_update)
+
+            update_minutes_query = f"UPDATE `user` SET `minutes_count` = '{minutes_to_update+minutes_count}' WHERE `email` = '{email}';"
+            cursor.execute(update_minutes_query)
+
+            # Model Running
+            output = replicate.run("openai/whisper:e39e354773466b955265e969568deb7da217804d8e771ea8c9cd0cef6591f8bc",
+                                   input={"audio": audioFile,
+                                          "model": "large-v2",
+                                          "transcription": "plain text",
+                                          "translate": to_translate,
+                                          #   "language": language,
+                                          "temperature": 0,
+                                          #   "patience": 1.0,
+                                          "suppress_tokens": "-1",
+                                          "initial_prompt": "",
+                                          "condition_on_previous_text": False,
+                                          "temperature_increment_on_fallback": 0.2,
+                                          "compression_ratio_threshold": 2.4,
+                                          "logprob_threshold": -1,
+                                          "no_speech_threshold": 0.6})
+
+            cnx.commit()
+            cursor.close()
+            cnx.close()
+            return render_template('whisper-results.html', data=[output['transcription'], output['translation'], output['detected_language'], minutes_count, minutes_total])
+
+        else:
+            # Counting audio file minutes
+            # from mutagen.wave import WAVE
+
+            # audios = WAVE(audioRecordedGlobal)
+            # audio_length = audios.info.length
+            # minutes = str(audio_duration(audio_length))
+            # minutes_to_update = float(minutes[0:4])
+
+            # update_minutes_query = f"UPDATE `user` SET `minutes_count` = '{minutes_to_update+minutes_count}' WHERE `email` = {email};"
+            # cursor.execute(update_minutes_query)
+
+            # Model Running
+            output = replicate.run("openai/whisper:e39e354773466b955265e969568deb7da217804d8e771ea8c9cd0cef6591f8bc",
+                                   input={"audio": audioRecords,
+                                          "model": "large-v2",
+                                          "transcription": "plain text",
+                                          "translate": to_translate,
+                                          #   "language": language,
+                                          "temperature": 0,
+                                          #   "patience": 1.0,
+                                          "suppress_tokens": "-1",
+                                          "initial_prompt": "",
+                                          "condition_on_previous_text": False,
+                                          "temperature_increment_on_fallback": 0.2,
+                                          "compression_ratio_threshold": 2.4,
+                                          "logprob_threshold": -1,
+                                          "no_speech_threshold": 0.6})
+
+            cnx.commit()
+            cursor.close()
+            cnx.close()
+            return render_template('whisper-results.html', data=[output['transcription'], output['translation'], output['detected_language'], minutes_count, minutes_total])
+            return "Done"
+    else:
+        return render_template('whisper-results.html', data=["", "", "", "", "", "You Have Used All Your Minutes"])
 
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
+
 @app.errorhandler(500)
 def internal_server(e):
     return render_template('500.html'), 500
-
 
 
 if __name__ == "__main__":
