ac38b2a8db3c4f378109d5d462b983bc95350525
developspeed <gerard@dataspeed.nl>
2023-04-28 16:04:21 +0200

    Add files via upload

--- static/js/image.js
+++ static/js/image.js
@@ -1,3 +1,4 @@
+// Taking Camera Input and Uploading the image to the server
 const captureButton = document.getElementById("capture");
 const image = document.getElementById("image");
 
@@ -8,8 +9,7 @@ captureButton.addEventListener("click", function () {
   video.style.display = "flex";
   captureButton.innerText = "Capture";
   let stream = null;
-  
-//   captureButton.addEventListener("click", () => {
+  captureButton.addEventListener("click", () => {
     const canvas = document.createElement("canvas");
     canvas.width = video.videoWidth;
     canvas.height = video.videoHeight;
@@ -17,74 +17,33 @@ captureButton.addEventListener("click", function () {
     const dataURL = canvas.toDataURL("image/png");
     image.style.display = "flex";
     photo.setAttribute("src", dataURL);
-    
+
     canvas.toBlob((blob) => {
       const formData = new FormData();
-      formData.append('imageFile', blob);
-      fetch('/upload-image', {
-        method: 'POST',
-        body: formData
+      formData.append("imageFile", blob);
+      fetch("/upload-image", {
+        method: "POST",
+        body: formData,
       })
-      .then(response => {
-        console.log("Done");
-      })
-      .catch(error => {
-        console.error(error);
-      });
-
-//     });
-
-
-    navigator.mediaDevices
-      .getUserMedia({ video: true })
-      .then((mediaStream) => {
-        stream = mediaStream;
-        video.srcObject = stream;
-        video.play();
-      })
-      .catch((error) => {
-        console.error("Unable to access camera", error);
+        .then((response) => {
+          console.log("Done");
+        })
+        .catch((error) => {
+          console.error(error);
+        });
     });
   });
-
+  navigator.mediaDevices
+    .getUserMedia({ video: true })
+    .then((mediaStream) => {
+      stream = mediaStream;
+      video.srcObject = stream;
+      video.play();
+    })
+    .catch((error) => {
+      console.error("Unable to access camera", error);
+    });
 });
-
-// const video = document.getElementById('video');
-// const canvas = document.getElementById('canvas');
-// const capture = document.getElementById('capture');
-
-// const constraints = {
-//   video: true
-// };
-
-// navigator.mediaDevices.getUserMedia(constraints)
-// .then((stream) => {
-//   video.srcObject = stream;
-// })
-// .catch((error) => {
-//   console.error(error);
-// });
-
-// capture.addEventListener('click', () => {
-//   canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
-//   canvas.toBlob((blob) => {
-//     const formData = new FormData();
-//     formData.append('image', blob, 'image.jpg');
-//     fetch('/upload-image', {
-//       method: 'POST',
-//       body: formData
-//     })
-//     .then(response => {
-//       console.log(response);
-//     })
-//     .catch(error => {
-//       console.error(error);
-//     });
-//   }, 'image/jpeg', 0.95);
-// });
-
-
-
 
 
 // For uploading and sending image to server
@@ -92,20 +51,21 @@ const previewImage = (event) => {
   const imageFiles = event.target.files;
   const imageFilesLength = imageFiles.length;
   if (imageFilesLength > 0) {
-      const imageSrc = URL.createObjectURL(imageFiles[0]);
-      const imagePreviewElement = document.querySelector("#preview-selected-image");
-      imagePreviewElement.src = imageSrc;
-      imagePreviewElement.style.display = "block";
-      console.log(imageFiles[0])
+    const imageSrc = URL.createObjectURL(imageFiles[0]);
+    const imagePreviewElement = document.querySelector(
+      "#preview-selected-image"
+    );
+    imagePreviewElement.src = imageSrc;
+    imagePreviewElement.style.display = "block";
+    console.log(imageFiles[0]);
   }
 
-  
   let formData = new FormData();
-  formData.append('imageFile',imageFiles[0])
-  fetch('/upload-image',{
-    method:"POST",
-    body: formData
-  }).then(response =>{console.log("Done")})
+  formData.append("imageFile", imageFiles[0]);
+  fetch("/upload-image", {
+    method: "POST",
+    body: formData,
+  }).then((response) => {
+    console.log("Done");
+  });
 };
-
-
