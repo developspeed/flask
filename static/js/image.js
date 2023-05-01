45522e64c5c30a6cd2e15b13fd1dc39668e9055e
developspeed <gerard@dataspeed.nl>
2023-05-01 09:25:52 +0200

    Add files via upload

--- static/js/image.js
+++ static/js/image.js
@@ -50,14 +50,18 @@ captureButton.addEventListener("click", function () {
 const previewImage = (event) => {
   const imageFiles = event.target.files;
   const imageFilesLength = imageFiles.length;
+  const imageFilesSize = ((imageFiles[0].size)/(1024*1024)).toFixed(2);
+  const size = document.getElementById('size');
   if (imageFilesLength > 0) {
     const imageSrc = URL.createObjectURL(imageFiles[0]);
     const imagePreviewElement = document.querySelector(
       "#preview-selected-image"
     );
+    size.style.display = 'flex';
+    size.innerHTML = "Size - "+imageFilesSize +" MB";
     imagePreviewElement.src = imageSrc;
     imagePreviewElement.style.display = "block";
-    console.log(imageFiles[0]);
+    // console.log(imageFiles[0]);
   }
 
   let formData = new FormData();
