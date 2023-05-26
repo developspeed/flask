d224f07b16a488dfc85d71c3e23ebc80822ad1e3
developspeed <gerard@dataspeed.nl>
2023-05-26 07:24:51 +0200

    Add files via upload

---
+++ dalleimage.py
@@ -0,0 +1,24 @@
+import openai
+from utitlities import DBUpdateARG, DBReadARG, DBRead
+
+openai.api_key = DBRead('dalle_image_generator','API_Key')
+
+def DalleImageAPI(prompt, numberOfImage, size, userSession):
+    try:
+        result = {}
+        image = openai.Image.create(
+            prompt=prompt,
+            n=int(numberOfImage),
+            size=size
+        )
+        images_count = int(DBReadARG("user", "images_count", "email", userSession,result))
+        updatedImage = images_count+int(numberOfImage)
+        DBUpdateARG('user','images_count',updatedImage,'email',userSession)
+        imagesurl = [item["url"] for item in image["data"]]
+        return imagesurl, updatedImage
+    except Exception as e:
+        images_count = int(DBReadARG("user", "images_count", "email", userSession,result))
+        return e , images_count
+
+# Testing 
+# print(DalleImageAPI('squirrel',3,'1024x1024','ger@dataspeed.nl'))
