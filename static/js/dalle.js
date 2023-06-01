9148c6000ab97bdfe67f8716b0555b497fdddb9a
developspeed <gerard@dataspeed.nl>
2023-06-01 16:16:21 +0200

    Add files via upload

--- static/js/dalle.js
+++ static/js/dalle.js
@@ -1,10 +1,10 @@
 // Taking input from the mic using webspeech recognition api
 
-const micIcon = document.getElementById("change");
+const micIcon = document.getElementById("change1");
 let isRecording = false;
 let recognition;
-// console.log('loaded')
 micIcon.addEventListener("click", toggleRecording);
+
 
 function toggleRecording() {
   if (isRecording) {
@@ -18,13 +18,13 @@ function toggleRecording() {
 function startRecording() {
   micIcon.style.color = 'red';
   recognition = new window.webkitSpeechRecognition() || new window.SpeechRecognition();
-//   recognition.lang = "en-US"; // Set the language for speech recognition
+  //   recognition.lang = "en-US"; // Set the language for speech recognition
   recognition.continuous = true;
   
   recognition.onstart = function () {
     // console.log("Recording started");
   };
-
+  
   recognition.onresult = function (event) {
     const transcript = event.results[0][0].transcript;
     let text = document.getElementById('prompt');
@@ -34,7 +34,7 @@ function startRecording() {
     // micIcon.click()
     // Handle the recognized speech transcript here
   };
-
+  
   recognition.start();
 }
 
@@ -44,8 +44,49 @@ function stopRecording() {
   // console.log("Recording stopped");
 }
 
-// let imageedit = document.getElementById('editImageFile');
 
-// imageedit.addEventListener('change',()=>{
-//   console.log(imageedit.files[0]);
-// })
+
+
+
+const micIcon2 = document.getElementById("change2");
+let isRecording2 = false;
+let recognition2;
+micIcon2.addEventListener("click", toggleRecording2);
+
+function toggleRecording2() {
+  if (isRecording2) {
+    stopRecording2();
+  } else {
+    startRecording2();
+  }
+  isRecording2 = !isRecording2;
+}
+
+function startRecording2() {
+  micIcon2.style.color = 'red';
+  recognition2 = new window.webkitSpeechRecognition() || new window.SpeechRecognition();
+//   recognition.lang = "en-US"; // Set the language for speech recognition
+  recognition2.continuous = true;
+  
+  recognition2.onstart = function () {
+    // console.log("Recording started");
+  };
+
+  recognition2.onresult = function (event) {
+    const transcript = event.results[0][0].transcript;
+    let text = document.getElementById('prompt2');
+    // console.log("Transcript:", transcript);
+    text.value = transcript;
+    stopRecording2()
+    // micIcon.click()
+    // Handle the recognized speech transcript here
+  };
+
+  recognition2.start();
+}
+
+function stopRecording2() {
+  micIcon2.style.color = "gray";
+  recognition2.stop();
+  // console.log("Recording stopped");
+}
