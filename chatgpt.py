a53437b59a844612d0cbf231dde7ed0a3c6d7af9
developspeed <gerard@dataspeed.nl>
2023-06-15 22:41:38 +0530

    Update chatgpt.py

--- chatgpt.py
+++ chatgpt.py
@@ -12,7 +12,7 @@ def ChatGPTAPI(prompt, userSession):
         model="gpt-3.5-turbo-16k",
         # "messages": [{"role": "user", "content": "Hello!"}],
         messages=[{"role": "user", "content": prompt}],
-        temperature=0.7,
+        temperature=1,
         # max_tokens=8000,
         top_p=1,
         frequency_penalty=0.5,
