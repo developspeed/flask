429e3bf049569d910adb36f393589f5937eb886b
developspeed <gerard@dataspeed.nl>
2023-07-03 19:23:55 +0200

    Update chatgpt.py

--- chatgpt.py
+++ chatgpt.py
@@ -9,7 +9,7 @@ def ChatGPTAPI(prompt, userSession):
     result = {}
     try:
         response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo-16k",
+        model="gpt-4",
         # "messages": [{"role": "user", "content": "Hello!"}],
         messages=[{"role": "user", "content": prompt}],
         temperature=1,
