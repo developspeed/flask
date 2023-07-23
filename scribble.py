30d970c2b0fd4e540191495ff67b474f4a84139a
developspeed <gerard@dataspeed.nl>
2023-07-23 18:58:22 +0530

    Update scribble.py

--- scribble.py
+++ scribble.py
@@ -9,8 +9,8 @@ def ScribbleAPI(image,prompt,userSession):
     result = {}
     try:
         output = replicate.run(
-            "nevernotsean/scribble-stories:c1f1f32ea543c5b6a8f3968f167ce02aff1e27cd25c1234f49c5bc15e556b19d",
-            input={"image": open(image,'rb'),'prompt':prompt}
+            "rossjillian/controlnet:795433b19458d0f4fa172a7ccf93178d2adb1cb8ab2ad6c8fdc33fdbcd49f477",
+            input={"image": open(image,'rb'),'prompt':prompt,'structure':'scribble'}
         )
         images_count = int(DBReadARG('user','images_count','email',userSession,result))
         images_count += 1
@@ -20,3 +20,4 @@ def ScribbleAPI(image,prompt,userSession):
         images_count = int(DBReadARG('user','images_count','email',userSession,result))
         return '',error, images_count
     
+
