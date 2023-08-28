f8dfc77b781770620afed171319abfa407940d8c
developspeed <gerard@dataspeed.nl>
2023-08-28 16:50:36 +0530

    Update chatdocs.py

--- chatdocs.py
+++ chatdocs.py
@@ -12,7 +12,7 @@ def construct_index(directory_path,userSession):
     chunk_size_limit = 1000
 
     prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit=chunk_size_limit)
-    llm_predictor = LLMPredictor(llm=ChatOpenAI(temperature=1, model_name="gpt-3.5-turbo-16k", max_tokens=num_outputs))
+    llm_predictor = LLMPredictor(llm=ChatOpenAI(temperature=0.5, model_name="gpt-3.5-turbo-16k", max_tokens=num_outputs))
     documents = SimpleDirectoryReader(directory_path).load_data()
     index = GPTSimpleVectorIndex(documents, llm_predictor=llm_predictor, prompt_helper=prompt_helper)
     index.save_to_disk(f'index_{userSession}.json')
@@ -21,12 +21,10 @@ def construct_index(directory_path,userSession):
 
 def get_answer_for_question(question, userSession):
     index_path = f'index_{userSession}.json'
+    user_folder = f"documents/{userSession}/"
+    index = construct_index(user_folder, userSession)
     try:
-        if os.path.exists(index_path):
-            index = GPTSimpleVectorIndex.load_from_disk(index_path)
-        else:
-            user_folder = f"documents/{userSession}/"
-            index = construct_index(user_folder, userSession)
+        index = GPTSimpleVectorIndex.load_from_disk(index_path)
         response = index.query(question, response_mode="compact")
         return response.response
     except Exception as error:
