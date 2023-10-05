11fc4c7ce25d162d4d8a4bbd4cf0fcb31f73aed6
developspeed <gerard@dataspeed.nl>
2023-10-05 08:53:16 +0530

    Update dalleedit.py

--- dalleedit.py
+++ dalleedit.py
@@ -1,6 +1,8 @@
 import openai
 from utitlities import DBUpdateARG, DBReadARG, DBRead
 
+openai.api_key = DBRead('dalle_image_generator','API_Key')
+
 def DalleImageEditAPI(image, prompt, numberOfImage, size, userSession):
     try:
         result = {}
