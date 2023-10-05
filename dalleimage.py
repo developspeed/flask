17f5e27362830ad17fc7fea5e68d093f9f16cee1
developspeed <gerard@dataspeed.nl>
2023-10-05 08:53:28 +0530

    Update dalleimage.py

--- dalleimage.py
+++ dalleimage.py
@@ -1,6 +1,7 @@
 import openai
 from utitlities import DBUpdateARG, DBReadARG, DBRead
 
+openai.api_key = DBRead('dalle_image_generator','API_Key')
 
 def DalleImageAPI(prompt, numberOfImage, size, userSession):
     try:
